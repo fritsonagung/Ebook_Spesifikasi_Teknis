e51ff7fb5fc06e2f9fce60e99deb170ed4486772
fritsonagung <agungfritson@gmail.com>
2019-09-19 08:48:04 +0700

    bisa pindah fragment

--- app/src/main/java/id/co/telkom/ebookspesifikasiteknis/ui/Beranda/BerandaFragment.java
+++ app/src/main/java/id/co/telkom/ebookspesifikasiteknis/ui/Beranda/BerandaFragment.java
@@ -8,12 +8,18 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
 
 import id.co.telkom.ebookspesifikasiteknis.Adapter.Adapter_Thumbnail_Item;
 import id.co.telkom.ebookspesifikasiteknis.Model.Model_Thumbnail;
 import id.co.telkom.ebookspesifikasiteknis.R;
+import id.co.telkom.ebookspesifikasiteknis.ui.OverviewFTTH.OverviewFTTHFragment;
+import id.co.telkom.ebookspesifikasiteknis.ui.SegementFeeder.SegmentFeederFragment;
+import id.co.telkom.ebookspesifikasiteknis.ui.SegmentDistribusi.SegmentDistribusiFragment;
+import id.co.telkom.ebookspesifikasiteknis.ui.SegmentDrop.SegmentDropFragment;
+import id.co.telkom.ebookspesifikasiteknis.ui.SimulasiPowerLink.SimulasiPowerLinkFragment;
 
 public class BerandaFragment extends Fragment {
 
@@ -23,6 +29,65 @@ public class BerandaFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_beranda, container, false);
 
+        ftth = root.findViewById(R.id.button_overview_ftth);
+        ftth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OverviewFTTHFragment overviewFTTHFragment = new OverviewFTTHFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(((ViewGroup)getView().getParent()).getId(), overviewFTTHFragment, "findThisFragment")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        feeder = root.findViewById(R.id.button_segment_feeder);
+        feeder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SegmentFeederFragment segmentFeederFragment = new SegmentFeederFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(((ViewGroup)getView().getParent()).getId(), segmentFeederFragment, "findThisFragment")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        distribusi = root.findViewById(R.id.button_segment_distribusi);
+        distribusi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SegmentDistribusiFragment segmentDistribusiFragment = new SegmentDistribusiFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(((ViewGroup)getView().getParent()).getId(), segmentDistribusiFragment, "findThisFragment")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        drop = root.findViewById(R.id.button_segment_drop);
+        drop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               SegmentDropFragment segmentDropFragment = new SegmentDropFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(((ViewGroup)getView().getParent()).getId(), segmentDropFragment, "findThisFragment")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        simulasi = root.findViewById(R.id.button_sim_power_link);
+        simulasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SimulasiPowerLinkFragment simulasiPowerLinkFragment = new SimulasiPowerLinkFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(((ViewGroup)getView().getParent()).getId(), simulasiPowerLinkFragment, "findThisFragment")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
 
         return root;
     }
